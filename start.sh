b91259820897e06a298977ff9f60286c5a062b4f
Rajeev <rajeev.86dev@gmail.com>
2025-03-22 19:55:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,7 @@
 uvicorn app.main:app --host 0.0.0.0 --port 10000
+
+import gdown
+
+url = "https://drive.google.com/file/d/1-0VpADZlJocxblOEZdiEwcz5tpBnNDyU/view?usp=drive_link"
+output = "model_SD.pkl"
+gdown.download(url, output, quiet=False)
