789cadc2053e852c5e7e6c626027bceaa778ccf4
Rajeev <rajeev.86dev@gmail.com>
2025-03-23 11:13:55 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -81,4 +81,4 @@ def recommend_crops(request: CropRequest):
     return {"ranked_crops": ranked_crops}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 7000)))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
