b9df4ebffff2f9aa5d5022662ded13f5e0f34e58
Rajeev <rajeev.86dev@gmail.com>
2025-03-23 16:38:38 +0530

    Update __init__.py

--- src/preprocess/__init__.py
+++ src/preprocess/__init__.py
@@ -1,3 +1,8 @@
-from .SD_preprocess import preprocess_data, preprocess_dtypes
-from .SC_preprocess import transform_preprocessor
-from .YR_preprocess import transform_preprocessing
+import os
+import sys
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(BASE_DIR)
+from src.preprocess.SC_preprocess import transform_preprocessor
+from src.preprocess.SD_preprocess import preprocess_data, preprocess_dtypes
+from src.preprocess.YR_preprocess import transform_preprocessing
