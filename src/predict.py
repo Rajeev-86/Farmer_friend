f677a0a8e9b110a5a8132f2becaa2b1c7c86f193
Rajeev <rajeev.86dev@gmail.com>
2025-03-23 11:34:37 +0530

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -6,9 +6,9 @@ from typing import Any, List
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(BASE_DIR)
-from src.preprocess.SC_preprocess import transform_preprocessor
-from src.preprocess.SD_preprocess import preprocess_data, preprocess_dtypes
-from src.preprocess.YR_preprocess import transform_preprocessing
+from preprocess.SC_preprocess import transform_preprocessor
+from preprocess.SD_preprocess import preprocess_data, preprocess_dtypes
+from preprocess.YR_preprocess import transform_preprocessing
 
 def predict_market_demand(data: dict[str, Any]):
 
