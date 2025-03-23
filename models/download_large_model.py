cbd84891d6a34fd3dd30b7335241129137e0d39a
Rajeev <rajeev.86dev@gmail.com>
2025-03-23 10:13:40 +0530

    Update download_large_model.py

--- models/download_large_model.py
+++ models/download_large_model.py
@@ -1,12 +1,17 @@
 import gdown
 import os
+import sys
 
-MODEL_URL = "https://drive.google.com/file/d/1rnoQhjp59h89j6vK2YjcqBYXTNwMc0jf/view?usp=drive_link"
-MODEL_PATH = "models/Demand_Predictor/model_SD.pkl"
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(BASE_DIR)
+
+MODEL_URL = "https://drive.google.com/uc?id=1rnoQhjp59h89j6vK2YjcqBYXTNwMc0jf"
+MODEL_PATH = os.path.join(BASE_DIR, 'models', 'Demand_Predictor', 'model_SD.pkl')
 
 def download_model():
     if not os.path.exists(MODEL_PATH):
         print("Downloading large model...")
+        os.makedirs(os.path.dirname(MODEL_PATH), exist_ok=True)
         gdown.download(MODEL_URL, MODEL_PATH, quiet=False)
     else:
         print("Model already exists.")
