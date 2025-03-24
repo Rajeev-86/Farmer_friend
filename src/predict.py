37ace8b88dcfe0ada0c908cf75e5ecd16b23ea16
Rajeev <rajeev.86dev@gmail.com>
2025-03-24 10:28:53 +0530

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -6,8 +6,8 @@ from typing import Any, List
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(BASE_DIR)
-from src.preprocess.YR_preprocess import transform_preprocessor
-from src.preprocess.YR_preprocess import preprocess_data, preprocess_dtypes
+from src.preprocess.SC_preprocess import transform_preprocessor
+from src.preprocess.SD_preprocess import preprocess_data, preprocess_dtypes
 from src.preprocess.YR_preprocess import transform_preprocessing
 
 custom_globals = {
